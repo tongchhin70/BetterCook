a817370780db6ab6382b4262ace4696aa5fed8d8
Quoc Ly <quocply@csu.fullerton.edu>
2026-04-11 14:33:49 -0700

    created a favorites table

--- backend/model.py
+++ backend/model.py
@@ -7,7 +7,7 @@ users = Table(
     metadata,
     Column("id", Integer, primary_key=True),
     Column("username", String(50), unique=True, nullable=False, index=True),
-    Column("password", String(255), nullable=False),
+    Column("password", String(255), nullable=False)
 )
 
 recipes = Table(
@@ -20,5 +20,18 @@ recipes = Table(
     Column("instructions", String(255), nullable=False),
     Column("prep_time", Integer, nullable=False),
     Column("cook_time", Integer, nullable=False),
-    Column("servings", Integer, nullable=False),
+    Column("servings", Integer, nullable=False)
+)
+
+favorites = Table(
+    "favorites",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String(255), nullable=False),
+    Column("description", String(255), nullable=True),
+    Column("ingredients", String(255), nullable=False),
+    Column("instructions", String(255), nullable=False),
+    Column("prep_time", Integer, nullable=False),
+    Column("cook_time", Integer, nullable=False),
+    Column("servings", Integer, nullable=False)
 )
